f35742c6577d1bcb3b9eee1288a9e8ebf7fd1e66
pcasaretto <pcasaretto@gmail.com>
2024-03-25 11:05:58 -0300

    update nix flake

--- flake.nix
+++ flake.nix
@@ -1,38 +1,30 @@
 {
   inputs = {
-    naersk.url = "github:nix-community/naersk/master";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
   };
-
-  outputs = { self, nixpkgs, utils, naersk }:
-    utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        naersk-lib = pkgs.callPackage naersk { };
-      in
-      {
-        defaultPackage = naersk-lib.buildPackage ./.;
-        devShell = with pkgs; mkShell {
-          buildInputs = [
-            cargo
-            rustc
-            rustfmt
-            pre-commit
-            rustPackages.clippy
-
-            cargo-deny
-
-            cargo-edit
-            cargo-watch
-
-            # Spelling and linting
-            codespell
-            ];
-          RUST_SRC_PATH = rustPlatform.rustLibSrc;
-          shellHook = ''
-            export DYLD_LIBRARY_PATH=${rustc}/lib:$DYLD_LIBRARY_PATH
-          '';
-        };
-      });
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          overlays = [ (import rust-overlay) ];
+          pkgs = import nixpkgs {
+            inherit system overlays;
+          };
+          rustToolchain = pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+        in
+          with pkgs;
+          {
+            devShells.default = mkShell {
+              buildInputs = [ rustToolchain ];
+            };
+          }
+      );
 }
